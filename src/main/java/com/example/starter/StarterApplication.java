c0c8e5592a05f1d8c315319aa10c2696e1c64896
gaebabja <hippo420@naver.com>
2019-12-03 16:55:31 +0900

    프로젝트생성 및 DB

---
+++ src/main/java/com/example/starter/StarterApplication.java
@@ -0,0 +1,15 @@
+package com.example.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan()
+public class StarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarterApplication.class, args);
+	}
+
+}
